ea891eb68addd96b563a3899f8ec6142e1cbd517
StefanskiSlawomir <89963259+StefanskiSlawomir@users.noreply.github.com>
2021-09-28 14:54:24 +0200

    Add files via upload

---
+++ pracownia programowania/4Bg1/kwadrat.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>kwadrat</title>
+  </head>
+  <body>
+    <h3>Kwadrat</h3>
+    <form  method="get">
+      <input type="text" name="sideA" placeholder="Podaj dlugosc boku a"><br><br>
+      <input type="submit"  value="Oblicz">
+
+    </form>
+  </body>
+</html>
