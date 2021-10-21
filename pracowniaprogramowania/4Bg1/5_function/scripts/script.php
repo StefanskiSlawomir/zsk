47398b89b92f08176e8e70399b71f82690cad344
StefanskiSlawomir <89963259+StefanskiSlawomir@users.noreply.github.com>
2021-10-21 08:36:16 +0200

    Update script.php

--- pracowniaprogramowania/4Bg1/5_function/scripts/script.php
+++ pracowniaprogramowania/4Bg1/5_function/scripts/script.php
@@ -2,7 +2,7 @@
   require_once '../functions/function.php';
   show();
   if (!empty($_POST['name'])) {
-    echo "Imię: ".stringvalidate($_POST['name']).", długość: ".strlen(stringvalidate($_POST['name']));
+    echo "Imię: ".stringvalidate($_POST['name'] ,4).", długość: ".strlen(stringvalidate($_POST['name'], 4));
 
   }
  ?>
