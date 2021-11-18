cd6371aadf52bb768bef671f9a2f34d9e422af22
StefanskiSlawomir <89963259+StefanskiSlawomir@users.noreply.github.com>
2021-11-18 08:39:58 +0100

    Add files via upload

--- pracowniaprogramowania/4Bg1/7_bazy_danych/scripts/adduser.php
+++ pracowniaprogramowania/4Bg1/7_bazy_danych/scripts/adduser.php
@@ -2,7 +2,8 @@
   if (!empty($_POST)) {
     foreach ($_POST as $key => $value) {
       if(empty($value)) {
-        header("location: ../4_bazy_tabela_insert.php?error=2&info=Wypełnij wszystkie pola w formularzu!&addUser=");
+        header("location: ../5_bazy_tabela_insert_update.php?error=2&info=Wypełnij wszystkie pola w formularzu!&addUser=");
+        // header("location: ../4_bazy_tabela_insert.php?error=2&info=Wypełnij wszystkie pola w formularzu!&addUser=");
         exit();
       }
     }
@@ -10,14 +11,17 @@
     $sql="INSERT INTO `users` (`id`, `cityid`, `name`, `surname`, `birthday`) VALUES (NULL, '$_POST[cityid]', '$_POST[name]', '$_POST[surname]', '$_POST[birthday]');";
     $connect->query($sql);
     if ($connect->affected_rows == 1) {
-      header("location: ../4_bazy_tabela_insert.php?error=2&info=Prawidłowo dodano użytkownika!");
+      header("location: ../5_bazy_tabela_insert_update.php?error=2&info=Prawidłowo dodano użytkownika!");
+      // header("location: ../4_bazy_tabela_insert.php?error=2&info=Prawidłowo dodano użytkownika!");
     }
     else {
-      header("location: ../4_bazy_tabela_insert.php?error=2&info=Nie dodano użytkownika!&addUser=");
+      header("location: ../5_bazy_tabela_insert_update.php?error=2&info=Nie dodano użytkownika!&addUser=");
+      // header("location: ../4_bazy_tabela_insert.php?error=2&info=Nie dodano użytkownika!&addUser=");
     }
     $connect->close();
   }
   else{
-    header("location: ../4_bazy_tabela_insert.php?error=2&info=Wypełnij wszystkie dane!&addUser=");
+    header("location: ../5_bazy_tabela_insert_update.php?error=2&info=Wypełnij wszystkie dane!&addUser=");
+    // header("location: ../4_bazy_tabela_insert.php?error=2&info=Wypełnij wszystkie dane!&addUser=");
   }
 ?>
--- pracowniaprogramowania/4Bg1/7_bazy_danych/scripts/delete.php
+++ pracowniaprogramowania/4Bg1/7_bazy_danych/scripts/delete.php
@@ -12,14 +12,17 @@ if (!empty($_GET['id'])) {
       Usunięto użytkownika o id: $_GET[id];
       <a href="../3_bazy_tabela_delete.php?deleteUser=$_GET[id]">Powrót</a>
     ALERT;*/
-    header("location: ../3_bazy_tabela_delete.php?error=0&info=Usunięto użytkownika o id: $_GET[id]");
+    // header("location: ../3_bazy_tabela_delete.php?error=0&info=Usunięto użytkownika o id: $_GET[id]");
+    header("location: ..5_bazy_tabela_insert_update.php?error=0&info=Usunięto użytkownika o id: $_GET[id]");
   }
   else {
     //header("location: ../3_bazy_tabela_delete.php?error=1");
-    header("location: ../3_bazy_tabela_delete.php?error=1&info=Nie udało się usunąć rekordu");
+    // header("location: ../3_bazy_tabela_delete.php?error=1&info=Nie udało się usunąć rekordu");
+    header("location: ../5_bazy_tabela_insert_update.php?error=1&info=Nie udało się usunąć rekordu");
   }
 }
 else {
-  header("location: ../3_bazy_tabela_delete.php");
+  // header("location: ../3_bazy_tabela_delete.php");
+  header("location: ../5_bazy_tabela_insert_update.php");
 }
 ?>
